5febc1a3cf54d5b7ae0daa6a6318a316255a1b76
hkitago <hkitago@gmail.com>
2025-07-20 10:52:48 +0900

    Refactor for data structure changes and add storage reset for pages not matched by content_scripts (e.g., Safari's Start Page)

--- Shared (Extension)/Resources/background.js
+++ Shared (Extension)/Resources/background.js
@@ -9,27 +9,20 @@ browser.runtime.onInstalled.addListener(async () => {
   }
 });
 
-// Handle messages received from content.js to store data in local storage
-browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "updatePageInfo") {
-    browser.storage.local.set({
-      [sender.tab.id]: {
-        selectedText: message.text,
-        pageTitle: message.title,
-        currentUrl: message.url
-      }
-    });
-  }
-});
-
 // Send a message to content.js on when switching tab
 browser.tabs.onActivated.addListener((activeInfo) => {
   browser.tabs.sendMessage(activeInfo.tabId, { action: 'getPageInfo' })
     .catch(error => console.error('Error getting page info:', error));
 });
 
-// Send a message to content.js when the tab's status is complete
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  // remove key from storage on Start Page
+  if (!tab.url?.startsWith('http://') && !tab.url.startsWith('https://')) {
+    browser.storage.local.remove(String(tabId));
+    return;
+  }
+
+  // Send a message to content.js when the tab's status is complete
   if (changeInfo.status === 'complete') {
     browser.tabs.sendMessage(tabId, { action: 'getPageInfo' })
       .catch(error => console.error('Error getting page info after update:', error));
@@ -41,16 +34,6 @@ browser.tabs.onRemoved.addListener((tabId, removeInfo) => {
   browser.storage.local.remove(tabId.toString())
     .catch(error => console.error('Error removing tab info from storage:', error));
 });
-
-// Clear the local storage when failing to read storage on Popover
-const initializeStorage = async () => {
-  try {
-    await browser.storage.local.clear();
-    console.log('Storage has been cleared');
-  } catch (error) {
-    console.error('Error clearing storage:', error);
-  }
-};
 
 const sendMessageToActiveTab = async (message) => {
   try {
@@ -65,15 +48,15 @@ const sendMessageToActiveTab = async (message) => {
   }
 };
 
-const refreshPageInfo = async () => {
-  await initializeStorage();
-  await sendMessageToActiveTab({ action: 'refreshPageInfo' });
-};
-
-browser.runtime.onMessage.addListener((message) => {
-  if (message.action === 'refreshPageInfo') {
-    refreshPageInfo().catch(error => {
-      console.error('Error in refreshPageInfo:', error);
+browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  // Handle messages received from content.js to store data in local storage
+  if (message.action === 'updatePageInfo') {
+    browser.storage.local.set({
+      [sender.tab.id]: {
+        selectedText: message.text,
+        pageTitle: message.title,
+        currentUrl: message.url
+      }
     });
   }
 });
